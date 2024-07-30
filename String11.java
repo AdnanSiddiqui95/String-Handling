83f1f0c8345a4037d8049c2b6ad4d150fc5b412d
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-30 19:34:40 +0530

    Add files via upload

---
+++ String11.java
@@ -0,0 +1,9 @@
+public class String11
+{
+	public static void main(String[] args) 
+	{
+		String str="Adnam";
+		System.out.println(str.replace('m', 'n'));
+	}
+
+}
---
+++ String12.java
@@ -0,0 +1,14 @@
+public class String12
+{
+	public static void main(String[] args) 
+	{
+		String str="Adnan,Siddiqui,Kamran,Zeeshan,Salman,Rehan";
+		String[]x=str.split(",");
+		
+		for(String res:x )
+		{
+			System.out.println(res);
+		}
+	}
+
+}
